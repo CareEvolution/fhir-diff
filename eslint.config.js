1c318c856fe7aa71d4eb7539f53d4d52d1b81afd
Scott Favre <favre@careevolution.com>
2024-04-29 10:31:35 -0400

    cleaning config stuff

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,16 @@
-import { eslintConfig } from "@careevolution/eslint-config-ce-frontend"
+import globals from "globals";
+
+import path from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+import pluginJs from "@eslint/js";
+
+// mimic CommonJS variables -- not needed if using CommonJS
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended});
 
 export default [
-  ...eslintConfig
-]
+  {languageOptions: { globals: globals.browser }},
+  ...compat.extends("standard-with-typescript"),
+];
