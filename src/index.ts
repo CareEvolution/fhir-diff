5d542a2acc66e2494a16d41e93300cc401dedf2e
Scott Favre <favre@careevolution.com>
2024-05-07 15:24:54 -0400

    export ResourceAndKey

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from './fhirBundlesMatch';
 export * from './models/keyStore';
 export * from './models/fhirMatch';
 export * from './models/fhirReferences';
+export * from './models/resourceAndKey';
